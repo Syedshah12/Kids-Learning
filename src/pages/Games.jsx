e7c90c890584737c6817226d165236242cf90bf0
unknown <abeershah392@gmail.com>
2024-12-27 00:30:51 +0500

    com

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -14,29 +14,54 @@ const Games = () => {
 <div className='bg-beige'>
 
 
-<div id='About' className='bg-beige'>
-    <div className="flex-col flex py-2">
-    <div className="flex lg:flex-row flex-col gap-2  px-4 py-4 lg:py-8  lg:px-8">
+<div id='About' className='bg-beige max-w-[1400px] mx-auto'>
+    <div className="flex-col flex">
+    <div className="flex lg:flex-row flex-col gap-24 px-4 py-4 lg:py-8  lg:px-8">
 
+
+{/* right */}
+{/* right */}
+<div className='block lg:hidden w-full lg:w-1/2  h-1/2 overflow-hidden  px-1 lg:px-0  '>
+<img
+
+// Smooth animation
+className='  object-cover  lg:max-h-[360px]'  src={wordgame} alt="" />
+
+
+</div>
 
 {/* left */}
-<div className='w-full  h-1/2 lg:w-1/2 '>
-<div className='px-4 lg:bg-gradient-to-l  gap-4 bg-gradient-to-b flex flex-col      '>
+<div className='w-full   lg:w-1/2 '>
+<div className='px-4  h-full    py-4 flex flex-col         '>
+
+
 <div><h1 className='text-xl text-[#F78A8F] font-bold'>Challenge Your Mind with Our Word Game!</h1></div>
-<div className='font-atma text-sm '>
+
+
+<div className='flex flex-col h-full  gap-12 py-6'>
+<div className='font-atma  '>
 Dive into the exciting world of words with our engaging word game. Whether you're a vocabulary wizard or just looking for a fun way to sharpen your language skills, this game offers endless entertainment. Race against the clock, uncover hidden words, and challenge yourself to beat your high score. Perfect for players of all ages, our word game is a great way to boost your creativity and keep your brain active. Start playing today and discover the joy of wordplay!
 </div>
 
 
+<div className='flex justify-center lg:justify-start'><Link to={'/wordgame'} className='bg-purple-400 px-12 py-2 rounded-sm font-semibold font-atma  text-white cursor-pointer hover:bg-purple-500 transition-all duration-200'>Play Now</Link></div>
 </div>
+
+
+
+</div>
+
 
 
 </div>
 
 
 {/* right */}
-<div className='w-full lg:w-1/2  bg-wordgame   bg-cover bg-center bg-no-repeat h-[200px] lg:h-auto   px-1 lg:px-0 '>
+<div className='hidden lg:block w-full lg:w-1/2  h-1/2 overflow-hidden  px-1 lg:px-0 '>
+<img
 
+// Smooth animation
+className='  object-cover w-full lg:max-h-[360px]'  src={wordgame} alt="" />
 
 
 </div>
@@ -45,41 +70,56 @@ Dive into the exciting world of words with our engaging word game. Whether you'r
 
 </div>
 
-<div className='w-full flex  justify-center'><Link to={'/wordgame'} className='bg-purple-400 px-12 py-2 rounded-sm font-semibold font-atma mb-8 text-white cursor-pointer hover:scale-105 transition-all duration-200'>Play Now</Link></div>
+
     </div>
 
 {/* game 2 */}
-<div className="flex-col flex my-2">
-    <div className="flex lg:flex-row flex-col gap-2  px-4 py-4 lg:py-8  lg:px-8">
+<div className="flex-col flex mt-2">
+    <div className="flex lg:flex-row gap-24 flex-col px-4 py-4 lg:py-8  lg:px-8">
 
 {/* right */}
-<div className='w-full lg:w-1/2  bg-patterngame    bg-center bg-no-repeat h-[200px] lg:h-auto   px-1 lg:px-0 '>
+<div className='w-full lg:w-1/2  h-1/2 overflow-hidden  px-1 lg:px-0 '>
+<img
 
+// Smooth animation
+className=' max-h-[360px]  w-full object-cover object-center '  src={patterngame} alt="" />
 
 
 </div>
 
 {/* left */}
-<div className='w-full  h-1/2 lg:w-1/2 '>
-<div className='px-4 lg:bg-gradient-to-l  gap-4 bg-gradient-to-b flex flex-col      '>
+<div className='w-full   lg:w-1/2 '>
+<div className='px-4  h-full    py-4 flex flex-col         '>
+
+
 <div><h1 className='text-xl text-[#F78A8F] font-bold'>Master the Art of Pattern Recognition!</h1></div>
-<div className='font-atma text-sm '>
+
+
+<div className='flex flex-col h-full gap-12 py-6'>
+<div className='font-atma  '>
 Test your observation and cognitive skills with our Pattern Recognition Game! This exciting challenge requires you to identify and match patterns within a set of images, numbers, or shapes. As the difficulty increases, so does the complexity of the patterns, pushing you to think faster and more creatively. Perfect for those who enjoy brain teasers and want to enhance their problem-solving abilities, this game offers endless fun while sharpening your pattern recognition skills. Ready to spot the pattern?
 </div>
 
 
+<div className='flex justify-center lg:justify-start'><Link to={'#'} className='bg-purple-400 px-12 py-2 rounded-sm font-semibold font-atma  text-white cursor-pointer hover:bg-purple-500 transition-all duration-200'>Play Now</Link></div>
 </div>
-
-
-</div>
-
-
 
 
 
 </div>
 
-<div className='w-full flex  justify-center'><Link to={'#'} className='bg-purple-400 px-12 py-2 rounded-sm font-semibold font-atma mb-8 text-white cursor-pointer hover:scale-105 transition-all duration-200'>Play Now</Link></div>
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+
     </div>
 </div>
 
